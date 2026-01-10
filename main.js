3640b12917b83eec59b2b335db521d71c49c6b18
LorenzoAerospace <lorenzo.biscotti@etik.com>
2026-01-10 10:49:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log('Hello World!');
+document.getElementById('searchInput').addEventListener('keyup', function() {
+    let filter = this.value.toLowerCase();
+    let cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        let title = card.querySelector('h3').innerText.toLowerCase();
+        let description = card.querySelector('p').innerText.toLowerCase();
+        
+        if (title.includes(filter) || description.includes(filter)) {
+            card.style.display = ""; // Mostra
+        } else {
+            card.style.display = "none"; // Nascondi
+        }
+    });
+});
+
